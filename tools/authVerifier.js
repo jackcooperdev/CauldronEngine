7d03042e1f050d50bb0055154437e0f2b50970d0
Jack Cooper <dev@jackcooper.me>
2024-09-01 17:11:06 +0100

    docs

--- tools/authVerifier.js
+++ tools/authVerifier.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
-const { cauldronLogger } = require('../tools/logger');
+const { cauldronLogger } = require('./logger');
 const fs = require('fs');
 const path = require('path');
-const {checkInternet } = require('../tools/checkConnection');
+const {checkInternet } = require('./checkConnection');
 
 // Verifies that the user owns a valid license of Minecraft
 
