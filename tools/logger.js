9608bc199372c860ecd2a15a7a6687137865b92f
Jack Cooper <dev@jackcooper.me>
2025-04-05 03:17:14 +0100

    :truck: Sync

--- tools/logger.js
+++ tools/logger.js
@@ -8,83 +8,90 @@ const host = '127.0.0.1';
 log4js.configure({
     appenders: {
         out: { type: "console" },
+        filtered: {
+            type: "noLogFilter",
+            exclude: "Listening For Minecraft Instances on port",
+            appender: "out",
+        },
         file: { type: "file", filename: path.join(grabPath(),'cauldron_engine_logs','logs.log'), maxLogSize: 10485760, backups: 3, compress: true},
-        network: { type: "tcp", host: "127.0.0.1",layout:{ type: "coloured" }, port:25568 },
+        //network: { type: "tcp", host: "127.0.0.1",layout:{ type: "coloured" }, port:25568 },
     },
-    categories: { default: { appenders: ["out","file","network"], level: "debug" } },
+    categories: { default: { appenders: ["filtered","file"], level: "debug" } },
 });
 
 const mcLogger = log4js.getLogger("Minecraft");
 const cauldronLogger = log4js.getLogger('Cauldron')
 
-let server;
-try {
-    server = net.createServer();
-    server.listen(port, host, () => {
-        cauldronLogger.info('Listening For Minecraft Instances on port ' + port + '.');
-    });
-} catch {
-    //do nothing 
-}
+function startMCListening() {
+    let server;
+    try {
+        server = net.createServer();
+        server.listen(port, host, () => {
+            cauldronLogger.info('Listening For Minecraft Instances on port ' + port + '.');
+        });
+    } catch {
+        //do nothing
+    }
 
 
-let sockets = [];
+    let sockets = [];
 
-let loggerSession = "";
+    let loggerSession = "";
 
-server.on('connection', function (sock) {
-    cauldronLogger.info('Minecraft Connected');
-    sockets.push(sock);
-    sock.on('data', function (data) {
-        let firstSplit = data.toString().split("\n");
-        for (let idx in firstSplit) {
-            if (firstSplit[idx] !== '') {
-                let dataMessage = firstSplit[idx].split("]: ");
-                if (dataMessage[1]) {
-                    let state = dataMessage[0].split("/")[1];
-                    dataMessage.shift();
-                    switch (state) {
-                        case 'INFO':
-                            mcLogger.info(dataMessage.join(" "));
-                            break;
-                        case 'DEBUG':
-                            mcLogger.debug(dataMessage.join(" "));
-                            break;
-                        case 'TRACE':
-                            mcLogger.trace(dataMessage.join(" "));
-                            break;
-                        case 'WARN':
-                            mcLogger.warn(dataMessage.join(" "));
-                            break;
-                        case 'ERROR':
-                            mcLogger.error(dataMessage.join(" "));
-                            break;
-                        default:
-                            mcLogger.error(dataMessage.join(" "));
-                            break;
+    server.on('connection', function (sock) {
+        cauldronLogger.info('Minecraft Connected');
+        sockets.push(sock);
+        sock.on('data', function (data) {
+            let firstSplit = data.toString().split("\n");
+            for (let idx in firstSplit) {
+                if (firstSplit[idx] !== '') {
+                    let dataMessage = firstSplit[idx].split("]: ");
+                    if (dataMessage[1]) {
+                        let state = dataMessage[0].split("/")[1];
+                        dataMessage.shift();
+                        switch (state) {
+                            case 'INFO':
+                                mcLogger.info(dataMessage.join(" "));
+                                break;
+                            case 'DEBUG':
+                                mcLogger.debug(dataMessage.join(" "));
+                                break;
+                            case 'TRACE':
+                                mcLogger.trace(dataMessage.join(" "));
+                                break;
+                            case 'WARN':
+                                mcLogger.warn(dataMessage.join(" "));
+                                break;
+                            case 'ERROR':
+                                mcLogger.error(dataMessage.join(" "));
+                                break;
+                            default:
+                                mcLogger.error(dataMessage.join(" "));
+                                break;
+                        }
                     }
                 }
             }
-        }
-    });
-    sock.on('close', function () {
-        let index = sockets.findIndex(function (o) {
-            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
-        })
-        if (index !== -1) sockets.splice(index, 1);
-        cauldronLogger.info('Minecraft Disconnected');
+        });
+        sock.on('close', function () {
+            let index = sockets.findIndex(function (o) {
+                return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+            })
+            if (index !== -1) sockets.splice(index, 1);
+            cauldronLogger.info('Minecraft Disconnected');
             destroySession(loggerSession).then(function (){});
             loggerSession = "";
-        
-    });
-});
 
-server.on('error', function (sock) {
-    cauldronLogger.error(sock);
-})
+        });
+    });
+
+    server.on('error', function (sock) {
+        cauldronLogger.error(sock);
+    })
+}
 
 function attachLoggerSession(id) {
     loggerSession = id;
 }
 
-module.exports = { cauldronLogger, attachLoggerSession}
+module.exports = { cauldronLogger, attachLoggerSession, startMCListening}
